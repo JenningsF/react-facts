f3a0f274b8d03e2b87c0a34163a80d77d081ccd6
JenningsF <jenningsf@protonmail.com>
2023-02-11 21:45:49 -0600

    Completed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import logo from "../images/logo.png"
 
 function Navbar() {
 	return (
 		<nav>
-			<h4>React Course - Project 1</h4>
+			<img src={logo} alt="React Logo" className="nav--icon" />
+			<h3 className="nav--logo_text">Reacts Facts</h3>
+			<h4 className="nav--title">React Course - Project 1</h4>
 		</nav>
 	);
 }
