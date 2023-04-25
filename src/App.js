6a105e8d667d44cf15dad05c2381fae142994cef
JenningsF <6432715+JenningsF@users.noreply.github.com>
2023-04-25 13:09:05 -0500

    Added styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
-import logo from "./logo.svg";
-import "./App.css";
+import "./style.css";
 
 function App() {
+	const [darkMode, setDarkMode] = React.useState(true);
+
+	function ToggleDarkMode() {
+		setDarkMode(prevMode => !prevMode)
+	}
+
 	return (
 		<div className="App">
 			<Navbar />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from "react";
+import reactLogo from "../images/react-logo.png";
 
-function Navbar() {
+function Navbar(props) {
 	return (
 		<nav>
-			<h4>React Course - Project 1</h4>
+			<img
+				className="nav--logo_icon"
+				src={reactLogo}
+				alt="ReactFacts Logo"
+			/>
+			<h3 className="nav--logo_text">ReactFacts</h3>
+			<h4 className="nav--title">Created by <a href="https://github.com/jenningsf" className="nav--title_link" target="_blank" rel="noreferrer">Jennings</a></h4>
 		</nav>
 	);
 }
